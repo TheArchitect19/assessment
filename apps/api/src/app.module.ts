a744d71b3a1afc63543be601d5eae8c5c3661c12
Ayush Kumar <ayush.krsh9@gmail.com>
2025-01-11 02:53:46 +0530

    feat: added user and auth module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { JwtModule } from '@nestjs/jwt';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.NODE_ENV ? `.${process.env.NODE_ENV}.env` : '.env',
+    }),
+    MongooseModule.forRoot(`${process.env.MONGO_URI}/${process.env.DB_NAME}`),
+    AuthModule,
+    JwtModule,
+    UserModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cors from 'cors';
+import * as bodyParser from 'body-parser';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cors());
+  app.use(bodyParser.json({ limit: '50mb' }));
+  app.use(bodyParser.raw({ limit: '50mb' }));
+  app.use(bodyParser.text({ limit: '50mb' }));
+  app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+  const config = new DocumentBuilder()
+    .setTitle('assessment')
+    .setDescription('API for the assessment.')
+    .setVersion('1.0')
+    .addTag('nestjs')
+    .addBearerAuth() // for auth
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(8000);
 }
 bootstrap();
